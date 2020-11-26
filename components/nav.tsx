26495ba26819d19ee397694f0e9f9fd51df426ac
Cezar Neaga <hi@cezarneaga.eu>
2020-11-26 10:11:14 +0100

    add darkmode styles to nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -31,7 +31,7 @@ const parties = [
     slug: "alde",
   },
 ];
-export default function Nav() {
+export function Nav() {
   const [show, setShow] = useState("");
   const [showNav, setShowNav] = useState(false);
   const router = useRouter();
@@ -40,18 +40,18 @@ export default function Nav() {
     setShow("");
   }, [activeRoute]);
   return (
-    <div className="relative bg-white">
+    <div className="relative bg-white dark:bg-gray-800">
       <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
         <div className="flex justify-start lg:w-0 lg:flex-1">
           <Link href="#">
-            <h1 className=" font-thin text-3xl ">Nu tot ei</h1>
+            <h1 className="font-thin text-3xl dark:text-white">Nu tot ei</h1>
           </Link>
         </div>
         <div className="-mr-2 -my-2 md:hidden">
           <button
             type="button"
             onClick={() => setShowNav(true)}
-            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            className="bg-white dark:bg-gray-800 rounded-md p-2 inline-flex items-center justify-center text-gray-400 dark:text-white dark:hover:text-gray-100 hover:text-gray-500 dark:hover:bg-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-100"
           >
             <span className="sr-only">Open menu</span>
             {/*Heroicon name: menu */}
@@ -76,11 +76,11 @@ export default function Nav() {
           <div className="relative">
             <button
               type="button"
-              className={`group bg-white rounded-md ${
+              className={`group bg-white dark:bg-gray-800 rounded-md ${
                 activeRoute === "/partide/[:slug]"
-                  ? "text-gray-900"
-                  : "text-gray-500"
-              } inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+                  ? "text-gray-900 dark:text-white"
+                  : "text-gray-500 dark:text-gray-200"
+              } inline-flex items-center text-base font-medium hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-100`}
               onClick={() => setShow((s) => (s === "partide" ? "" : "partide"))}
               onMouseEnter={() => setShow("partide")}
               onMouseLeave={() => {
@@ -93,9 +93,9 @@ export default function Nav() {
               <svg
                 className={`ml-2 h-5 w-5 ${
                   activeRoute === "/partide/[:slug]"
-                    ? "text-gray-600"
-                    : "text-gray-400"
-                } group-hover:text-gray-500`}
+                    ? "text-gray-600 dark:text-white"
+                    : "text-gray-400 dark:text-gray-600"
+                } group-hover:text-gray-500 dark:group-hover:text-gray-100`}
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
@@ -125,12 +125,12 @@ export default function Nav() {
                   className="absolute left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0"
                 >
                   <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="z-20 relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                    <div className="z-20 relative grid gap-6  bg-white dark:bg-gray-800 px-5 py-6 sm:gap-8 sm:p-8">
                       {parties.map((party) => (
                         <a
                           key={party.slug}
                           href={`/candidati/${party.slug}`}
-                          className="-m-3 p-3 flex rounded-md hover:bg-gray-50"
+                          className="-m-3 p-3 flex rounded-md hover:bg-gray-50 dark:hover:bg-gray-900"
                         >
                           <div className="flex-shrink-0 flex mt-1 items-center justify-center h-10 w-10 rounded-md">
                             <Image
@@ -139,11 +139,11 @@ export default function Nav() {
                               width={40}
                             />
                           </div>
-                          <div className="ml-4 text-base font-medium text-gray-900">
-                            <p className="text-base font-medium text-gray-900">
+                          <div className="ml-4 text-base font-medium">
+                            <p className="text-base font-medium text-gray-900 dark:text-gray-100">
                               {party.partyShort}
                             </p>
-                            <p className="mt-1 text-sm text-gray-500">
+                            <p className="mt-1 text-sm text-gray-500 dark:text-gray-200">
                               {party.party}
                             </p>
                           </div>
@@ -159,8 +159,10 @@ export default function Nav() {
           <Link href="/despre">
             <span
               className={`text-base cursor-pointer font-medium ${
-                activeRoute === "/despre" ? "text-gray-900" : "text-gray-500"
-              } hover:text-gray-900`}
+                activeRoute === "/despre"
+                  ? "text-gray-900 dark:text-white"
+                  : "text-gray-500 dark:text-gray-200"
+              } hover:text-gray-900 dark:hover:text-gray-100`}
             >
               Despre
             </span>
@@ -168,8 +170,10 @@ export default function Nav() {
           <Link href="/cautare">
             <span
               className={`text-base cursor-pointer font-medium ${
-                activeRoute === "/cautare" ? "text-gray-900" : "text-gray-500"
-              } hover:text-gray-900`}
+                activeRoute === "/cautare"
+                  ? "text-gray-900 dark:text-white"
+                  : "text-gray-500 dark:text-gray-200"
+              } hover:text-gray-900 dark:hover:text-gray-100`}
             >
               Căutare
             </span>
@@ -179,13 +183,13 @@ export default function Nav() {
           <a
             href="https://facebook.com/valeriu.nicolae.39"
             target="_blank"
-            className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            className="whitespace-nowrap text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100"
           >
             <Facebook />
           </a>
           <a
             href="mailto:valeriu@nutotei.ro"
-            className="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            className="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100"
           >
             <Mail />
           </a>
@@ -243,6 +247,7 @@ export default function Nav() {
                   <nav className="grid grid-cols-2 gap-7">
                     {parties.map((party) => (
                       <a
+                        key={party.slug}
                         href={`/candidati/${party.slug}`}
                         className="-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50"
                       >
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Nav from '../components/nav'
+import Layout from "components/layout";
 
-export default function IndexPage() {
+export default function IndexPage({ preview }) {
   return (
-    <div>
-      <Nav />
+    <Layout preview={preview}>
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
-    </div>
-  )
+    </Layout>
+  );
 }
