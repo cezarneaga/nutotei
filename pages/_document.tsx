d4034ddfe84cce4cab7189464fe20b85e17d50f7
Cezar Neaga <hi@cezarneaga.eu>
2020-11-30 22:38:16 +0100

    remove fonts loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,11 +32,6 @@ class MyDocument extends Document {
             }}
           />
           <link rel="icon" href="/images/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&family=Open+Sans:wght@300;400;600&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
