0fee799c09eba9614c89d6824f07a8349abaf070
Mihai Sampaleanu <mihai.sampaleanu@gmail.com>
2020-12-05 14:25:09 +0200

    fix og:url for map

--- pages/harta/[slug].tsx
+++ pages/harta/[slug].tsx
@@ -60,6 +60,8 @@ export default function IndexPage({
     return mapdata
   }
 
+  const { slug } = router.query
+
   return (
     <Layout preview={preview}>
       {router.isFallback ? (
@@ -69,9 +71,9 @@ export default function IndexPage({
             <NextSeo
               title={`Harta - Nu tot ei!`}
               description={`Harta pe judete`}
-              canonical={`https://nutotei.ro/harta`}
+              canonical={`https://nutotei.ro/harta/${slug}`}
               openGraph={{
-                url: `https://nutotei.ro/harta`,
+                url: `https://nutotei.ro/harta/${slug}`,
                 title: `Harta - Nu tot ei!`,
                 description: `Harta pe judete`,
                 images: [{ url: 'https://nutotei.ro/images/nutotei.png' }]
