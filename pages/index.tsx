65c16e7afcab012e5a1aaee8a7177e01b2888249
Catalin Apostu <dev.catalin.apostu@gmail.com>
2022-03-13 20:23:53 +0100

    Embed infogram

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,18 +49,11 @@ export default function IndexPage({
             data-title='Raport 1 v6'
           ></div>
           <Script
-            id='embed-infogram'
-            strategy='afterInteractive'
+            id=''
             dangerouslySetInnerHTML={{
               __html: `
                 !function(e,i,n,s){var t="InfogramEmbeds",d=e.getElementsByTagName("script")[0];if(window[t]&&window[t].initialized)window[t].process&&window[t].process();else if(!e.getElementById(n)){var o=e.createElement("script");o.async=1,o.id=n,o.src="https://e.infogram.com/js/dist/embed-loader-min.js",d.parentNode.insertBefore(o,d)}}(document,0,"infogram-async");
               `,
-            }}
-            onLoad={() => {
-              console.log('Embedded infogram')
-            }}
-            onError={(e) => {
-              console.log('error: ', e)
             }}
           />
         </div>
