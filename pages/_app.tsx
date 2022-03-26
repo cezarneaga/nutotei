4e7e514f0eabf60bc4a95af802fe9cf3df92b4a8
Catalin Apostu <dev.catalin.apostu@gmail.com>
2022-03-27 00:02:23 +0100

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,50 @@ import '../styles/index.css'
 import type { AppProps /*, AppContext */ } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import SEO from '../next-seo.config'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+
+    //When the component is mounted, subscribe to router changes and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -9,19 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang='ro'>
         <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
           <link rel='icon' href='/images/favicon.ico' />
         </Head>
         <body>
